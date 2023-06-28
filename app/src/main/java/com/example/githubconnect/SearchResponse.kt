70a0250e554558e2bc88cfec0bae5600124379ee
mahendramin <mahendramin19@gmail.com>
2023-06-28 11:48:01 +0700

    Create SearchResponse and use it in RecyclerView Adapter

---
+++ app/src/main/java/com/example/githubconnect/SearchResponse.kt
@@ -0,0 +1,30 @@
+package com.example.githubconnect
+
+import com.google.gson.annotations.SerializedName
+
+data class SearchResponse(
+
+    @field:SerializedName("total_count")
+    val totalCount: Int,
+
+    @field:SerializedName("incomplete_results")
+    val incompleteResults: Boolean,
+
+    @field:SerializedName("items")
+    val items: List<ItemsItem>
+)
+
+data class ItemsItem(
+
+    @field:SerializedName("login")
+    val username: String,
+
+    @field:SerializedName("type")
+    val type: String,
+
+    @field:SerializedName("avatar_url")
+    val avatarUrl: String,
+
+    @field:SerializedName("id")
+    val id: Int,
+)
---
+++ app/src/main/java/com/example/githubconnect/UserAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.githubconnect
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.githubconnect.databinding.ItemUserListBinding
+
+class UserAdapter(private val listUser: List<ItemsItem>) :
+    RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+    inner class UserViewHolder(val binding: ItemUserListBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val binding =
+            ItemUserListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = listUser.size
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.binding.apply {
+            tvUsername.text = listUser[position].username
+            tvUserAccountType.text = listUser[position].type
+            Glide.with(holder.itemView.context)
+                .load(listUser[position].avatarUrl)
+                .into(imgAvatar)
+        }
+    }
+}
